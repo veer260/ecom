79483ca0811b0db5039fef325a312c3e4ac193f8
veer260 <57021124+veer260@users.noreply.github.com>
2023-01-13 16:51:17 +0530

    Added initial validations using yup

--- src/Components/LoginPage.jsx
+++ src/Components/LoginPage.jsx
@@ -1,19 +1,28 @@
 import { useFormik } from 'formik';
 import React from 'react'
+import * as Yup from 'yup';
 
 const LoginPage = () => {
 
     function callLoginApi(values) {
+        console.log('email', touched.email);
+        console.log('password', touched.password)
         console.log('sending data', values.email, values.password); 
     }
 
-    const { handleSubmit, handleChange, values, resetForm} = useFormik({
+    const schema = Yup.object().shape( {
+        email: Yup.string().email().required(),
+        password: Yup.string().min(8, 'bhut chota password h bhai').max(12, 'bhut lmba password h bhai').required(),
+    })
+
+    const { handleSubmit, handleChange, values, resetForm, errors, handleBlur, touched, isValid} = useFormik({
         initialValues: {
             email: '',
             password: ''
         },
-        onSubmit: callLoginApi
-    })
+        onSubmit: callLoginApi,
+        validationSchema: schema
+    }) 
 
 
   return (
@@ -28,6 +37,7 @@ const LoginPage = () => {
                 <input 
                 value={values.email}
                 onChange={handleChange}
+                onBlur={handleBlur}
                 name='email'
                 type="email" 
                 placeholder='Email address '
@@ -36,22 +46,30 @@ const LoginPage = () => {
                 required
                 className='p-2 border-2 rounded-md placeholder-gray-300'
                 />
+                {touched.email && errors.email && (
+                    <h3 className='text-primary-dark '>{errors.email}!</h3>
+                )}
             </div>
             <div>
                 <label htmlFor="MyPassword"></label>
                 <input 
                 value={values.password}
                 onChange={handleChange}
+                onBlur={handleBlur}
                 placeholder='Password'
                 name='password'
                 className='p-2 border-2 rounded-md placeholder-gray-300'
                 id='MyPassword'
                 type="password" />
+                {touched.password && errors.password && (
+                    <h3 className='text-primary-dark '>{errors.password}!</h3>
+                )}
+
             </div>
             <button
             onClick={resetForm} 
             className='bg-primary-default py-1 px-3 rounded-md uppercase text-white mt-4'>Reset Form</button>
-            <button className='bg-primary-default py-1 px-3 rounded-md uppercase font-semibold text-white mt-8'>Login</button>
+            <button className='bg-primary-default py-1 px-3 rounded-md uppercase font-semibold text-white mt-8 disabled:bg-primary-light' disabled={!isValid}>Login</button>
         </form>
     </div>
   )
