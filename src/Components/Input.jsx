6c8626e86367ad37cc00bda619928b773687715f
veer260 <57021124+veer260@users.noreply.github.com>
2023-01-14 13:16:29 +0530

    Replaced useFormik hook with Formik component

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,19 +1,31 @@
+import { useField } from 'formik'
 import React from 'react'
 
-const Input = ({id, label, touched, error, ...rest}) => {
+const Input = ({id, name, label, className, ...rest}) => {
+  
+  const field = useField(name);
+  const [data, meta] = field;
+  const {onBlur, onChange, value } = data;
+  const { touched, error} = meta;
+  const errorClass = (touched && error) ? className : "border-gray-300 focus:border-gray-100"
+  // console.log(data);
   return (
     <div>
-         <div className='items-center justify-center '>
-                <label htmlFor={id} className='sr-only' >{label}</label>
-                <input 
-                {...rest}
-                id={id}
-                className='p-2 border-2 rounded-md placeholder-gray-300'
-                />
-                {touched && error && (
-                    <h3 className='text-primary-dark '>{error}!</h3>
-                )}
-            </div>
+      <div className='items-center justify-center '>
+        <label htmlFor={id} className='sr-only' >{label}</label>
+        <input 
+        {...rest}
+        name={name}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+        id={id}
+        className={'p-2 border-2 focus:border-gray-200 rounded-md outline-none placeholder-gray-300 appearence-none '+ errorClass}
+        />
+        {touched && error && (
+            <h3 className='text-primary-dark '>{error}!</h3>
+        )}
+      </div>
     </div>
   )
 }
--- src/Components/LoginPage.jsx
+++ src/Components/LoginPage.jsx
@@ -1,10 +1,9 @@
-import { useFormik } from 'formik';
+import { Formik, Form } from 'formik';
 import React from 'react'
 import * as Yup from 'yup';
 import Input from './Input';
 
 const LoginPage = () => {
-
     function callLoginApi(values) {
         console.log('email', touched.email);
         console.log('password', touched.password)
@@ -13,59 +12,44 @@ const LoginPage = () => {
 
     const schema = Yup.object().shape( {
         email: Yup.string().email().required(),
-        password: Yup.string().min(8, 'bhut chota password h bhai').max(12, 'bhut lmba password h bhai').required(),
+        password: Yup.string().min(8).max(12).required(),
     })
 
-    const { handleSubmit, handleChange, values, resetForm, errors, handleBlur, touched, isValid} = useFormik({
-        initialValues: {
-            email: '',
-            password: ''
-        },
-        onSubmit: callLoginApi,
-        validationSchema: schema,
-        validateOnMount: true
-    }) 
-
+    const initialValues = {
+        email: '',
+        password: ''
+    };
 
   return (
     <div className='flex flex-col items-center justify-center h-screen font-semibold space-y-4'>
-        
-        <form
-            onSubmit={handleSubmit} 
+        <Formik initialValues={initialValues} onSubmit={callLoginApi} validationSchema={schema} validateOnMount={true} >
+        <Form 
             action="" className='flex flex-col bg-white border p-8  justify-center items-center'>
             <h1 className='text-2xl'>Login page to CodeYogi</h1>
             <Input
             id="MyEmail"
-            value={values.email}
             name="email"
             placeholder='Email address'
             type="email"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            touched={touched.email}
-            error={errors.email}
             required={true}
             label="Email address"
+            className='border-primary-default b-2'
             />
 
             <Input
             id="MyPassword"
-            value={values.password}
             name="password"
             placeholder='Password'
             type="password"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            touched={touched.password}
-            error={errors.password}
             required={true}
             label="Password"
             />
             <button
-            onClick={resetForm} 
+            // onClick={resetForm} 
             className='bg-primary-default py-1 px-3 rounded-md uppercase text-white mt-4'>Reset Form</button>
-            <button className='bg-primary-default py-1 px-3 rounded-md uppercase font-semibold text-white mt-8 disabled:bg-primary-light' disabled={!isValid}>Login</button>
-        </form>
+            <button className='bg-primary-default py-1 px-3 rounded-md uppercase font-semibold text-white mt-8 disabled:bg-primary-light' >Login</button>
+        </Form>
+        </Formik>
     </div>
   )
 }
